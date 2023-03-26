cd19c4310dbcdb3dcb6634b295ee196387229ffb
Dean Lozo <dean.lozo@technipfmc.com>
2023-03-26 19:59:12 +0200

    Removed and re-ordered annotations

--- src/test/kotlin/no/lozo/zonkypresentation/office/OfficeControllerTest.kt
+++ src/test/kotlin/no/lozo/zonkypresentation/office/OfficeControllerTest.kt
@@ -6,7 +6,6 @@ import io.zonky.test.db.AutoConfigureEmbeddedDatabase.RefreshMode.*
 import java.util.UUID
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
